b0a2f54154cf5a72e0e6badb5fea828b87e93fce
lindabettini <linda.bettini@hotmail.it>
2021-12-21 16:09:06 +0100

    Add const kmRun

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ console.log('JS OK');
 
 /* 
 1.Chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-2.Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+2.Calcolare il prezzo totale del viaggio, secondo queste regole:
 il prezzo del biglietto è definito in base ai km (0.21 € al km).
 3/4. Va applicato uno sconto del 20% per i minorenni
 3/4. Va applicato uno sconto del 40% per gli over 65.
 5.L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). */
+
+// Chiedere all'utente quanti km vuole percorrere
+const kmRun = parseInt(prompt('Quanti chilometri vuoi percorrere?')); 
+console.log(kmRum);
