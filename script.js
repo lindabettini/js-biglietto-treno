45f1e262211dbfd8ab05edbc5a089e84f64fb384
lindabettini <linda.bettini@hotmail.it>
2021-12-21 15:17:33 +0100

    Add track comment

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log('JS OK');
+
+/* 
+1.Chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+2.Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km).
+3/4. Va applicato uno sconto del 20% per i minorenni
+3/4. Va applicato uno sconto del 40% per gli over 65.
+5.L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). */
