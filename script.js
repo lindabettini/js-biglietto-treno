7901ed4eab2cbbf2734834521345bf4a2ddce0da
lindabettini <linda.bettini@hotmail.it>
2021-12-21 16:34:06 +0100

    Add discounts

--- script.js
+++ script.js
@@ -8,10 +8,24 @@ il prezzo del biglietto è definito in base ai km (0.21 € al km).
 3/4. Va applicato uno sconto del 40% per gli over 65.
 5.L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). */
 
-//1. Chiedere all'utente quanti km vuole percorrere
+// Chiedere all'utente quanti km vuole percorrere
 const kmRun = parseInt(prompt('Quanti chilometri vuoi percorrere?')); 
 console.log(kmRun);
 
-//2. Calcolare il prezzo totale del viaggio
-const fullPrice = kmRun * 0.21
-console.log('il costo totale è di: €', 'fullPrice')
+// Chiedere all'utente quanti km vuole percorrere
+const passengerAge = parseInt(prompt('Quanti anni hai?')); 
+console.log(passengerAge);
+
+//Definire sconti
+const minor = 20
+const over65 = 40
+
+// Calcolare il prezzo totale del viaggio
+let price = kmRun * 0.21;
+
+if (passengerAge < 18){
+    let price: calcolaPerc(fullPrice,minor);
+  
+}
+
+console.log('il costo totale è di: €', 'price')
