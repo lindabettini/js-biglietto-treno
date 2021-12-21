d9d30b5cdfc1295503f80357822f7697b46d3707
lindabettini <linda.bettini@hotmail.it>
2021-12-21 16:17:42 +0100

    Add const fullPrice

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ il prezzo del biglietto è definito in base ai km (0.21 € al km).
 3/4. Va applicato uno sconto del 40% per gli over 65.
 5.L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). */
 
-// Chiedere all'utente quanti km vuole percorrere
+//1. Chiedere all'utente quanti km vuole percorrere
 const kmRun = parseInt(prompt('Quanti chilometri vuoi percorrere?')); 
-console.log(kmRum);
+console.log(kmRun);
+
+//2. Calcolare il prezzo totale del viaggio
+const fullPrice = kmRun * 0.21
+console.log('il costo totale è di: €', 'fullPrice')
